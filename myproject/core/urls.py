007468ff5ee79be6695500ab294b16701ecc6d88
herbety <herbetyp@gmail.com>
2019-08-21 02:19:49 -0300

    Update like

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -6,4 +6,6 @@ app_name = 'core'
 urlpatterns = [
     path('', v.index, name='index'),
     path('videos/add/', v.VideoCreate.as_view(), name='video_add'),
+    path('videos/<int:pk>/like/', v.video_like, name='video_like'),
+    path('videos/<int:pk>/unlike/', v.video_unlike, name='video_unlike'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,7 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from myproject.core.models import Video
-from myproject.core.forms import VideoForm
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView
+from myproject.core.forms import VideoForm
+from myproject.core.models import Video
+
 
 def index(request):
     videos = Video.objects.all()
@@ -14,3 +17,27 @@ class VideoCreate(CreateView):
     template_name = 'add.html'
     form_class = VideoForm
     success_url = '/'
+
+
+@csrf_exempt
+def video_like(request, pk):
+    if request.method == 'POST' and request.is_ajax():
+        data = request.POST
+        like = data.get('like')
+        video = Video.objects.get(pk=pk)
+        video.like = like
+        video.save()
+        response = {'data': video.like}
+        return JsonResponse(response)
+
+
+@csrf_exempt
+def video_unlike(request, pk):
+    if request.method == 'POST' and request.is_ajax():
+        data = request.POST
+        unlike = data.get('unlike')
+        video = Video.objects.get(pk=pk)
+        video.unlike = unlike
+        video.save()
+        response = {'data': video.unlike}
+        return JsonResponse(response)
