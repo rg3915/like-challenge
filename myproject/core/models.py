1b94352972e3bb37ea559ee35ecc5cd120d896ce
herbety <herbetyp@gmail.com>
2019-08-21 00:20:54 -0300

    create models

--- myproject/core/models.py
+++ myproject/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Theme(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Video(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
+    like = models.PositiveIntegerField(default=0)
+    unlike = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
